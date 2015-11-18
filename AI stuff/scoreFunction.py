d65fba58b7d1a2f6cafc9f96bab1be4209c5f545
brywalker1 <brywalker1@gmail.com>
2015-11-17 22:39:43 -0700

    Added AI Stuff
    This was one example, in python, of using the ray tracing to solve a
    complex problem

---
+++ AI stuff/scoreFunction.py
@@ -0,0 +1,34 @@
+﻿import numpy as np
+from rayTracer import Ray
+
+def score(rays, apeturePoints, viewingAngles, apetureRadius):
+
+    overall_score = 0.;
+    numNans = 0;
+    for i in range(len(apeturePoints)):
+        subRays,_ = rays.apeture(apetureRadius, apeturePoints[i]);
+        if subRays is None:
+            subscore = -10;
+        else:
+            maxes = np.max(np.dot(subRays.direction.T, viewingAngles ),axis = 0);#axis = 1 gives us the best angle for a ray, axis 0 gives us the best ray for the angle
+            subscore = np.sum(maxes);
+
+        if np.isnan(subscore):
+            numNans += 1;
+        else:
+            overall_score += subscore;
+
+    return overall_score
+
+"""import numpy as np
+from apeture import apeture
+def score(rays, apeturePoints, viewingAngles, apetureRadius):
+
+    radius = 3;
+    score = 0;
+    for i in range(len(apeturePoints)):
+        subRays = apeture(rays, apetureRadius, apeturePoints[i]);
+        score += np.sum(np.max(np.dot(viewingAngles, subRays),axis = 1));
+
+    return score
+    """
---
+++ Python RayTracer/change.py
@@ -0,0 +1,45 @@
+﻿from rayTracer import Ray
+#from rayTracer import unitVector
+#import random
+import numpy as np
+s = np.sin(np.divide(0.0625,(-128,-64,-32,-16,-8,-4,-2,-1,np.inf,1,2,4,8,16,32,64,128)))
+c = np.cos(np.divide(0.0625,(-128,-64,-32,-16,-8,-4,-2,-1,np.inf,1,2,4,8,16,32,64,128)))
+
+
+
+def change(rays, changeRadius):
+
+
+    x = np.random.randint(0,17);
+    y = np.random.randint(0,17);
+    z = np.random.randint(0,17);
+    changeMat = np.array([  [            c[y]*c[z]      ,           -c[y]*s[z]      ,     s[y]  , 0],
+                            [ c[x]*s[z] + c[z]*s[x]*s[y], c[x]*c[z] - s[x]*s[y]*s[z], -c[y]*s[x], 0],
+                            [ s[x]*s[z] - c[x]*c[z]*s[y], c[z]*s[x] + c[x]*s[y]*s[z],  c[x]*c[y], 0],
+                            [                0          ,                0          ,      0    , 1]  ]);
+
+    numRays = rays.direction.shape[1];
+    changeCentral = np.random.randint(0,numRays);
+    
+    #turns out, we want to change rays that are near each other, not far apart.
+    shift2origin = rays.position - np.vstack(rays.position[:,changeCentral]);
+    dist2 = np.sum(shift2origin*shift2origin, axis = 0);
+    rays2Change = (dist2 <= changeRadius*changeRadius)
+
+    rays.direction[:,rays2Change] = np.dot(changeMat, rays.direction[:,rays2Change]);
+    
+    return rays
+
+"""Matrix Rotations:
+   rotateX2 = [1   0   0   0;
+               0   c[x] -s[x]  0;
+               0   s[x]  c[x]  0;
+               0   0   0   1];
+   rotateY2 = [c[y]  0   s[y]  0;
+               0   1   0   0;
+              -s[y]  0   c[y]  0;
+               0   0   0   1];
+   rotateZ2 = [c[z] -s[z]  0   0;
+               s[z]  c[z]  0   0;
+               0   0   1   0;
+               0   0   0   1];"""
